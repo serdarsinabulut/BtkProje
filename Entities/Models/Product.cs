fea7260ff4335067f39861e897ad7a3aea2d0c82
serdarelsinao <serdarsina2861@gmail.com>
2025-02-18 14:53:44 +0300

    ..

---
+++ Entities/Models/Product.cs
@@ -0,0 +1,7 @@
+﻿namespace Entities.Models;
+public class Product
+{
+    public int ProductID { get; set; }
+    public String? ProductName { get; set; } = String.Empty;
+    public decimal Price { get; set; }
+}
