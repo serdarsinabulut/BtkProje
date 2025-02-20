4571e5e41da6c83632bc4113e41371c77fb3e00a
serdarelsinao <serdarsina2861@gmail.com>
2025-02-20 22:18:51 +0300

    Repositorymanager

--- Repositories/obj/Debug/net6.0/Repositories.AssemblyInfo.cs
+++ Repositories/obj/Debug/net6.0/Repositories.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Repositories")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cbed66970115fcd6edd7d9d1dc140c69497feb3e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ff0a3d8aa3f49d21e547055352def0e076d6191")]
 [assembly: System.Reflection.AssemblyProductAttribute("Repositories")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Repositories")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- btkproje/Controllers/ProductController.cs
+++ btkproje/Controllers/ProductController.cs
@@ -1,7 +1,7 @@
 using Entities.Models;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using Repositories;
+using Repositories.Contracts;
 
 
 namespace StoreApp.Controllers
@@ -9,23 +9,22 @@ namespace StoreApp.Controllers
     public class ProductController : Controller
     {
         
-        private readonly RepositoryContext _context;
+        private readonly IRepositoryManager _manager;
 
-        public ProductController(RepositoryContext context)
+        public ProductController(IRepositoryManager manager)
         {
-            _context = context;
+            _manager = manager;
         }
-
         public IActionResult Index()
         {
-            var model = _context.Products.ToList();
+            var model = _manager.Product.GetAllProducts(false);
             return View(model);
         }
 
         public IActionResult Get(int id)
         {
-            Product product = _context.Products.First(p=>p.ProductID.Equals(id));
-            return View(product);
+           //Product product = _context.Products.First(p=>p.ProductID.Equals(id));
+            throw new NotImplementedException();
         }
     }
 }
